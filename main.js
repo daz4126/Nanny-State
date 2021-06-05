6cbeb0fe2c7205b8780d17a5d192c52e2cdc5f46
DAZ <daz4126@gmail.com>
2021-06-05 14:18:03 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+import {html , render} from 'https://unpkg.com/lit-html?module'
+// Nanny State function
+const Nanny = (state={},{view=``,element=document.body,before,after}={}) => {
+  // initial render
+  render(view(state),element);
+  // return function used to update the state
+  return (action,...params) => {
+    // call before function here
+    if(before) before(state);
+    // create a new state based on the action and params submitted
+    const newState = Object.prototype.toString.call(state) === "[object Object]"
+                     ? { ...state,...action(state,...params) }
+                     : action(state,...params);
+    // set the new state
+    state = newState;
+    // call after function here
+    if(after) after(state);
+    // render the new state
+    render(view(state),element);
+    // return the new state
+    return state;
+  }
+}
