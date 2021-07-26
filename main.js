e9a5271a999b8a577be1a1d7feef7baf2aeb271e
DAZ <daz4126@gmail.com>
2021-07-26 10:03:57 +0100

    Update main.js
    refer to transformer functions rather than actions
    just updated state directly instead of creating newState variable first
    added some comments

--- main.js
+++ main.js
@@ -5,16 +5,14 @@ const Nanny = (state={},{ element=document.body,view=state.view,before,after,deb
   render(view(state),element);
   // log the state if in debug mode
   if(debug || logState) console.log(state);
-  // return function used to update the state
-  return (action,...params) => {
+  // return the update function
+  return (transformer,...params) => {
     // call before function here
     if(before) before(state);
-    // create a new state based on the action and params submitted
-    const newState = Object.prototype.toString.call(state) === "[object Object]"
-                     ? { ...state,...action(state,...params) }
-                     : action(state,...params);
-    // set the new state
-    state = newState;
+    // update the state based on the transformer function and params submitted
+    state = Object.prototype.toString.call(state) === "[object Object]" // check if the state is an object
+                     ? { ...state,...transformer(state,...params) } // shallow copy of the current state + any changes (should it be a deep copy?)
+                     : transformer(state,...params);
     // call after function here
     if(after) after(state);
     // render the new state
