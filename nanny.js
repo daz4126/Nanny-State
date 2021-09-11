953c1cbddbcedc93fd7992ad2d5c48260cb85a85
DAZ <daz4126@gmail.com>
2021-09-11 17:09:19 +0100

    Fixed another issue in nanny.jsgit add .

--- nanny.js
+++ nanny.js
@@ -9,7 +9,7 @@ function Nanny(
     after = state.after,
     debug = state.debug,
     logState = state.logState,
-    localStateKey = state.localStateKey
+    localStorageKey = state.localStorageKey
   } = {}
 ) {
   // Retrieve state from local storage.
