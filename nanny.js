389e6c004178d15242913c8f5c9451ed9d8982d1
DAZ <daz4126@gmail.com>
2022-03-05 17:15:34 +0000

    Fixed problem with template being set

--- nanny.js
+++ nanny.js
@@ -4,7 +4,8 @@ function Nanny(
   State = {},
   {
     Element = State.Element || document.body,
-    Template = State.Layout || State.View || `NANNY STATE`,
+    Layout = State.Layout,
+    View = State.View,
     Initiate = State.Initiate,
     Before = State.Before,
     After = State.After,
@@ -13,13 +14,14 @@ function Nanny(
     Render = State.Render || true
   } = {}
 ) {
+  const Template = Layout || View || "NANNY STATE"
   // Retrieve state from local storage.
   if(LocalStorageKey) {
     State = localStorage.getItem(LocalStorageKey) ? {...State,...JSON.parse(localStorage.getItem(LocalStorageKey))} : State;
   }
-
   // Set value of Content if required
   State.Content = typeof State[State.View] === "function" ? State[State.View](State) : ""
+  // Run any setup code once
   if(Initiate) {
     State =
       Object.prototype.toString.call(State) === "[object Object]"
