3104fc29a63c97faf7abcbaebea8dddec3ea6988
DAZ <daz4126@gmail.com>
2022-04-18 15:10:06 +0100

    removed optional chaining

--- nanny.js
+++ nanny.js
@@ -20,9 +20,9 @@ function Nanny(
   const findRoute = path => (path === '/' ? ['/'] : path.split('/').filter(char => char !== ''))
   .reduce(
     (obj, path, i) => {     
-     const param = obj?.routes?.find(r => r.path[0] === ':');
-     return obj?.routes?.find(r => r.path === path)
-        ? {...obj?.routes?.find(r => r.path === path),params: obj.params}
+     const param = obj.routes.find(r => r.path[0] === ':');
+     return obj.routes.find(r => r.path === path)
+        ? {...obj.routes.find(r => r.path === path),params: obj.params}
         : { ...param, params: {...obj.params,[param.path.slice(1)]: path} }
     },{ routes: Routes }
   );
@@ -30,8 +30,8 @@ function Nanny(
   function Render() {
     if(Routes.length){
       const route = findRoute(Path);
-      document.title = route?.title || document.title
-      if (route?.update) {
+      document.title = route.title || document.title
+      if (route.update) {
         const newState = route.params
           ? route.update(route.params)(State)
           : route.update(State);
@@ -41,7 +41,7 @@ function Nanny(
             : newState
           : State;
       }
-      if(StateIsObject && route?.view) State.Content = route?.view(State);
+      if(StateIsObject && route.view) State.Content = route.view(State);
     }
     render(Element, View(State));
   }
