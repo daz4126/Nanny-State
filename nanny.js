35caad6f249e4212e3d40d03ffc54ce3eab0e018
DAZ <daz4126@gmail.com>
2022-06-02 17:53:40 +0100

    Cut out lots of loc. Only objects for State

--- nanny.js
+++ nanny.js
@@ -1,25 +1,15 @@
 import {html, svg, render} from 'uhtml';
 
-function Nanny(
-  State = {},
-  {
-    Element = State.Element || document.body,
-    View = State.View,
-    Initiate = State.Initiate,
-    Before = State.Before,
-    After = State.After,
-    Debug = State.Debug,
-    Routes = State.Routes || [],
-    Path = window.location.pathname,
-    LocalStorageKey = State.LocalStorageKey
-  } = {}
-){
-  // Helper functions
-  const StateIsObject = Object.prototype.toString.call(State) === "[object Object]";
+function Nanny(State){
+  // Default settings
+  const Element = State.Element || document.body,
+        Routes = State.Routes || [],
+        Path = window.location.pathname
 
+  // Helper function to find the route object in the state given the path
   const findRoute = path => (path === '/' ? ['/'] : path.split('/').filter(char => char !== ''))
   .reduce(
-    (obj, path, i) => {     
+    (obj, path) => {     
      const param = obj.routes.find(r => r.path[0] === ':');
      return obj.routes.find(r => r.path === path)
         ? {...obj.routes.find(r => r.path === path),params: obj.params}
@@ -28,102 +18,72 @@ function Nanny(
   );
 
   function Render() {
-    if(Routes.length){
+    if (Routes.length){
       const route = findRoute(Path);
       document.title = route.title || document.title
       if (route.update) {
-        const newState = route.params
-          ? route.update(route.params)(State)
-          : route.update(State);
-        State = newState
-          ? StateIsObject
-            ? { ...State, ...newState }
-            : newState
-          : State;
+        State = { ...State, ...(route.params ? route.update(route.params)(State) : route.update(State)) };
       }
-      if(StateIsObject && route.view) State.Content = route.view(State);
+      if (route.view) {
+        State.Content = route.view(State);
+      }
     }
     render(Element, View(State));
   }
 
   // Retrieve state from local storage.
-  if(LocalStorageKey && localStorage.getItem(LocalStorageKey)) {
-    State =  StateIsObject
-        ? {...State,...JSON.parse(localStorage.getItem(LocalStorageKey))} 
-        : JSON.parse(localStorage.getItem(LocalStorageKey));
+  if (State.LocalStorageKey && localStorage.getItem(State.LocalStorageKey)) {
+    State =  {...State,...JSON.parse(localStorage.getItem(State.LocalStorageKey))} 
   }
 
-  // append Route method to State
-  if(Routes && StateIsObject){
-    State.Route = path => event => {
-      event.preventDefault();
-      Path = path || event.target.attributes.href.value;
-      window.history.pushState({ Path }, Path, `${Path}`);
-      Render()
-    };
-
-    window.addEventListener("popstate", event => {
-      Path = window.location.pathname;
-      Render();
-    });
+  if (Routes.length) {
+      // append Route method to State
+      State.Route = path => event => {
+        event.preventDefault();
+        Path = path || event.target.attributes.href.value;
+        window.history.pushState({ Path }, Path, `${Path}`);
+        Render();
+      };
+      // grab the path from the address bar
+      window.addEventListener("popstate", event => {
+        Path = window.location.pathname;
+        Render();
+      });
   }
 
   // Run any setup code once
-  if(Initiate) {
-    const newState = Initiate(State);
-    State = newState
-      ? StateIsObject
-        ? { ...State, ...newState }
-        : newState
-      : State;
+  if (State.Initiate) {
+    State = { ...State, ...State.Initiate(State) };
   }
 
   // Render view based on initial state.
   Render();
 
-  if (Debug) {
-    console.log(State);
+  if (State.Debug) {
+    console.log(JSON.stringify(State));
   }
 
-  return (transformer) => {
-    if (Before) {
-      const newState = Before(State);
-      State = newState
-        ? StateIsObject
-          ? { ...State, ...newState }
-          : newState
-        : State;
+  return (newState) => {
+    if (State.Before) {
+      State = { ...State, ...State.Before(State) };
     }
 
-    // Update state based on the arguments.
-    const newState =
-      typeof transformer === "function"
-        ? transformer(State)
-        : transformer;
+    State = { ...State, ...(typeof newState === "function" ? newState(State) : newState) };
 
-    State = StateIsObject
-        ? { ...State, ...newState }
-        : newState;
-
-    if (After) {
-      const newState = After(State);
-      State = newState
-        ? StateIsObject
-          ? { ...State, ...newState }
-          : newState
-        : State;
+    if (State.After) {
+      State = { ...State, ...State.After(State) };
     }
 
+    if (State.LocalStorageKey){
+      localStorage.setItem(State.LocalStorageKey,JSON.stringify(State));
+    }
+
+    if (State.Debug) {
+      console.log(JSON.stringify(State));
+    } 
+    
     // Re-render the view based on updated state
     Render();
-
-    if(LocalStorageKey){
-      localStorage.setItem(LocalStorageKey,JSON.stringify(State));
-    }
-
-    if (Debug) {
-      console.log(State);
-    }
 
     return State;
   };
