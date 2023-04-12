528338a3b1e0882bc3de45398bc79ba1563a3b4c
DAZ <daz4126@gmail.com>
2023-04-12 07:46:35 +0100

    localState blacklist, Effect, Calculate methods

--- nanny.js
+++ nanny.js
@@ -1,16 +1,11 @@
 import {html, svg, render} from 'uhtml';
 
-export default function Nanny(State, Path = window.location.pathname,Routes = State.Routes || []){  
+export default function Nanny(State, Path = window.location.pathname,Routes = State.Routes || [], Effects = [],Calcs = []){
   // Retrieve state from local storage.
   if (State.LocalStorageKey && localStorage.getItem(State.LocalStorageKey)) {
     State =  {...State,...JSON.parse(localStorage.getItem(State.LocalStorageKey))} 
   }
-  // Run any setup code once
-  if (State.Initiate) {
-    State = { ...State, ...State.Initiate(State)};
-  }
-  // Set intial State
-  State = { ...State, ...(State.Calculate ? State.Calculate(State) : {}) };
+  
   // Built-In State Methods
   State.Evaluate = prop => prop === undefined ? {...State} : {...State}[prop];
   State.JSON = () => JSON.stringify(State);
@@ -28,38 +23,54 @@ export default function Nanny(State, Path = window.location.pathname,Routes = St
   });
   State.Every = (moment,...transformers) => setInterval(_ => State.Update(...transformers),moment);
   State.Delay = (moment,...transformers) => setTimeout(_ => State.Update(...transformers),moment);
+  State.Effect = (effect,list) => {
+    if(!Effects.some(e => e[0].toString() === effect.toString() && e[1] === list)) Effects.push([effect,list]);
+  }
+  State.Calculate = (calc,list) => {
+    if(!Calcs.some(c => c[0].toString() === calc.toString() && c[1] === list)) Calcs.push([calc,list]);
+  }
   State.Update = (...transformers) => {
-    if (State.Before) {
-      State = { ...State, ...State.Before(State),...(State.Calculate ? State.Calculate(State.Before(State)) : {}) };
-    }
+    if (State.Before) setState(State.Before);
     
-    State = transformers.reduce((oldState,transformer) => {
-      const {Update,HTML,SVG,Evaluate,JSON,Link,Every,Delay,Content,...newState} = typeof(transformer) === "function" ? transformer(oldState) : transformer;
-      Object.entries(newState).forEach(([prop,value]) => value && value.toString() === "[object Object]" ? newState[prop] = {...State[prop],...value} : value)
-      const updatedState = { ...oldState, ...newState, ...(State.Calculate ? State.Calculate({...oldState,...newState}) : {}) };
-      if(State.Effects) {
-        State.Effects.filter(effect => !effect[1] || effect[1].split(",").some(prop => newState.hasOwnProperty(prop))).forEach(effect => effect[0](updatedState));
-      }
-      return updatedState
-    },State);
+    setState(...transformers);
     
-    if (State.After) {
-      State = { ...State, ...State.After(State), ...(State.Calculate ? State.Calculate(State.After(State)) : {}) };
-    }
+    if (State.After) setState(State.After);
 
     if (State.LocalStorageKey){
-      localStorage.setItem(State.LocalStorageKey,JSON.stringify(State));
+      const localState = {...State};
+      if(State.LocalStorageBlackList){
+        State.LocalStorageBlackList.split(",").forEach(key => delete localState[key]);
+      }
+      localStorage.setItem(State.LocalStorageKey,JSON.stringify(localState));
     }
 
-    if (State.Debug) {
-      console.log(State.JSON());
-    } 
-    
-    // Re-render the view basd on updated state
+    if (State.Debug) console.log(State.JSON());
+   
     Render();
   };
+  
+  // Run any setup code once and then render the initial state
+  if (State.Initiate) setState(State.Initiate)
+  State.View(State);
+  setState(State);
+  if (State.Debug) console.log(State.JSON());
+  Render();
        
-  // Helper function to find the route object in the state given the path
+  // Helper functions
+  function update(oldObj,newObj){
+    Object.entries(newObj).forEach(([prop,value]) => oldObj[prop]  = value)
+  }
+  
+  function setState(...transformers){
+    State = transformers.reduce((state,transformer) => {
+      const {Update,HTML,SVG,Evaluate,JSON,Link,Every,Delay,Content,Effect,Calculate,...newState} = typeof(transformer) === "function" ? transformer(state) : transformer;
+      update(state,newState);
+      Effects.filter(effect => !effect[1] || effect[1].split(",").some(prop => newState.hasOwnProperty(prop))).forEach(effect => effect[0](state));    
+      Calcs.filter(calc => !calc[1] || calc[1].split(",").some(prop => newState.hasOwnProperty(prop))).forEach(calc => update(state,calc[0](state)));
+      return state
+    },State);
+  }
+  
   const findRoute = path => (path === '/' ? ['/'] : path.split('/').filter(char => char !== ''))
   .reduce(
     (obj, path) => {     
@@ -75,7 +86,7 @@ export default function Nanny(State, Path = window.location.pathname,Routes = St
       const route = findRoute(Path);
       document.title = route.title || document.title
       if (route.update) {
-        State = { ...State, ...(route.params ? route.update(route.params)(State) : route.update(State)), ...(State.Calculate ? State.Calculate(route.params ? route.update(route.params)(State) : route.update(State)) : {}) };
+        setState(route.params ? route.update(route.params) : route.update);
       }
       if (route.view) {
         State.Content = route.view(State);
@@ -83,10 +94,6 @@ export default function Nanny(State, Path = window.location.pathname,Routes = St
     }
     render(State.Element || document.body, State.View(State));
   }
-  // Render view based on initial state.
-  State.Update(State);
   
   return State.Update
 }
-
-export { Nanny, html, svg }
